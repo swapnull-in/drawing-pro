2c5e9b60254082de381c74cd428c853270f55488
Swapnil Godambe <swapnil.go20@gmail.com>
2026-09-06 10:17:38 +0530

    Update compileSdk and targetSdk to API 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,12 +10,12 @@ plugins {
 
 android {
     namespace = "com.swapnull.drawingpro"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.swapnull.drawingpro"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0.0"
 
